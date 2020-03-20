640ce504500af1240377d2ef1f0088608b60216c
dbroeder <dbroeder@4rhuskies.org>
2020-03-20 14:03:34 -0500

    removed files

--- src/scenes/second-scene.js
+++
@@ -1,9 +0,0 @@
-export class SecondScene extends Phaser.Scene{
-    constructor(){
-        super({key: 'SecondScene'})
-    }
-
-    create(){
-        this.add.text(10,10,'Welcome to the second level')
-    }
-}
