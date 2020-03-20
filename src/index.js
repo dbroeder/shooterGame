9239b8524e37852feb0078120a2ecfd13ab6f9ec
dbroeder <dbroeder@4rhuskies.org>
2020-03-20 13:44:35 -0500

    personalized updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import 'phaser';
 
-import { SimpleScene } from './scenes/simple-scene';
+import { Scene1 } from './scenes/Scene1';
+import { Scene2 } from './scenes/Scene2';
 
-const gameConfig = {
-  width: 680,
-  height: 400,
-  scene: SimpleScene
-};
 
-new Phaser.Game(gameConfig);
+export var gameSettings = {
+	playerSpeed: 200,
+	maxPowerups: 2,
+	powerUpVel: 50,
+  }
+  
+  var config = {
+	width: 256,
+	height: 272,
+	backgroundColor: 0x000000,
+	scene: [Scene1, Scene2],
+	pixelArt: true,
+	physics: {
+	  default: "arcade",
+	  arcade:{
+		  debug: false,
+		  debugShowVelocity: false
+	  }
+	}
+  }
+  
+  
+  var game = new Phaser.Game(config);
--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -1,10 +1,59 @@
 export class SimpleScene extends Phaser.Scene {
+  constructor(){
+    super({key:'scene1'})
+    
+  }
+  
   preload() {
     this.load.image('cokecan', 'assets/cokecan.png');
+    this.load.image('straw','assets/kisspng-drinking-straw-toy-balloon-plastic-canudo-5b2d67365665c2.0864657515297021983539.png')
+    this.load.image('block','assets/rectangle.png')
   }
 
   create() {
-    this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
-    this.add.image(100, 200, 'cokecan');
+    this.score=0;
+    this.straw=this.physics.add.staticImage(200,100,'straw')
+    //this.straw.setCollideWorldBounds(true)
+    this.straw.setScale(1)
+    this.player =this.physics.add.sprite(10,10,'cokecan')
+    this.player.setCollideWorldBounds(true)
+    this.player.setBounce(0.2)
+    this.player.setScale(0.5)
+    this.cursors= this.input.keyboard.createCursorKeys();
+    this.physics.add.overlap(this.player,this.straw,(player,straw)=>{
+      console.log("hit")
+      straw.destroy()
+    })
+    this.blocks = this.physics.add.staticGroup()
+    this.blocks.create(100,300,'block')
+    this.blocks.create(400,100,'block')
+    
+    this.physics.add.collider(this.player,this.blocks)
+    console.log(this.cameras.main.width)
+
+  }
+  updateScore(){
+    this.add.text(100, 100, 'jumps: '+this.score, { fill: '#0f0' });
+  }
+  update(){
+    this.blocks.children.iterate((block)=>{
+      block.y++
+      block.refreshBody()
+      if(block.y==this.cameras.main.height){
+        block.x=100
+        block.y=100
+      }
+    })
+    if(this.cursors.right.isDown){
+      this.player.setVelocityX(200)
+    }else if(this.cursors.left.isDown){
+      this.player.setVelocityX(-200)
+    }else(
+      this.player.setVelocityX(0)
+    )
+    if(this.cursors.space.isDown){
+      this.player.setVelocityY(-500)
+    }
+    
   }
 }
